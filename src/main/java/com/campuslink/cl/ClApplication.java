2628ea462280ee228b92d5a23e1c49b85858aace
rexparmar <parmarrex114@gmail.com>
2025-12-04 13:48:12 -0500

    Initial project setup.

---
+++ src/main/java/com/campuslink/cl/ClApplication.java
@@ -0,0 +1,13 @@
+package com.campuslink.cl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campuslink/cl/ClApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuslink.cl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
